5cbafe62e4027ad195b7e4abce892d0ac8153966
RJ_Minal <rj.minera@gmail.com>
2018-10-31 09:58:07 -0600

    refactor: genres.model imports

--- models/genre.model.js
+++ models/genre.model.js
@@ -1,8 +1,8 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const Joi = require('joi');
+const mongoose = require('mongoose');
 
 const Genre = mongoose.model(
-  "Genre",
+  'Genre',
   new mongoose.Schema({
     name: {
       type: String,
@@ -19,6 +19,59 @@ function validateGenre(genre) {
       .min(5)
       .max(50)
       .required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+function validatePatient(genre) {
+  const schema = {
+    id: Joi.number().integer(),
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    last_name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    phone: Joi.string()
+      .min(5)
+      .max(20)
+      .required(),
+    dpi: Joi.string()
+      .length(13)
+      .required(),
+    address: Joi.string()
+      .min(5)
+      .max(100)
+      .required(),
+    email: Joi.string()
+      .email()
+      .min(5)
+      .max(50)
+      .required(),
+    age: Joi.number()
+      .integer()
+      .min(1)
+      .max(120)
+      .required(),
+    incription_date: Joi.string()
+      .length(10)
+      .required(),
+    id_clinic: Joi.number()
+      .min(1)
+      .required(),
+    id_ocupation: Joi.number()
+      .min(1)
+      .required(),
+    id_avatar: Joi.number()
+      .min(1)
+      .required(),
+    id_country: Joi.number()
+      .min(1)
+      .required(),
+    active: Joi.boolean().default(1)
   };
 
   return Joi.validate(genre, schema);
